d983db1fc8a28b62a85fb1602e5a7cd2c8885c09
Jonas Kim <youngmki@amazon.com>
2023-10-13 13:22:25 +0900

    misc

--- src/py_dreambooth/__version__.py
+++ src/py_dreambooth/__version__.py
@@ -2,7 +2,7 @@ __title__ = "py-dreambooth"
 __description__ = (
     "Fine-tuning the Stable Diffusion model and creating your own AI avatars"
 )
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Youngmin Kim"
 __author_email__ = "aldente0630@gmail.com"
 __license__ = "creativeml-openrail-m"
